335460df97b70fe9ad49511021f64c6d1ddcbf96
GrantRT <granto_92@hotmail.com>
2022-02-22 13:45:19 +0000

    generated readme based on user inputs

---
+++ Generated-README.md
@@ -0,0 +1,27 @@
+
+  # test1
+
+  ## Description
+  the is a test
+
+  ##  Installation instructions
+  here is how to instsall
+
+  ## Usage information 
+  heere is how to use
+
+  ## Contributing guidelines 
+  contributing factorrs
+
+  ## Test instructions
+  how to test
+
+  ## License
+  MIT License
+
+  ## Github username 
+  grantrt
+
+  ## Email address
+  
+
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -12,7 +12,32 @@ function renderLicenseSection(license) {}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `
+  # ${data.title}
+
+  ## Description
+  ${data.description}
+
+  ##  Installation instructions
+  ${data.installation}
+
+  ## Usage information 
+  ${data.usage}
+
+  ## Contributing guidelines 
+  ${data.contributing}
+
+  ## Test instructions
+  ${data.test}
+
+  ## License
+  ${data.license}
+
+  ## Github username 
+  ${data.github}
+
+  ## Email address
+  ${data.email}
 
 `;
 }
